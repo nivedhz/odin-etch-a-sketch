19ccfecf63e0f0bb12496732aca5b4d7a8e65aae
Nivedh E R <nivedher12@gmail.com>
2026-03-27 20:14:43 +0530

    add(core): implemented the basic structure
    -- Implemented the basic divs in html
    -- Implemented the createGrids() function in js
    -- Implemented the basic styles in css

--- script/script.js
+++ script/script.js
@@ -1,19 +1,21 @@
-function createGrids(row, coloumn) {
-  const container = document.getElementById("grid-container")
-  container.style.display = "flex"
-  container.style.flexWrap = "wrap"
-
-  for (let i = 0; i < row * coloumn; i++) {
-    const gridItem = document.createElement("div")
-    const itemWidth = 100 / coloumn
-    gridItem.style.width = `${itemWidth}%`
-    gridItem.style.border = "1px solid black"
-    gridItem.style.boxSizing = "border-box"
-    gridItem.style.textAlign = "center"
-
-    container.appendChild(gridItem)
-  }
-}
-
-
-createGrids(16, 16)
+// function createGrids(row, coloumn) {
+//   const container = document.getElementById("grid-container")
+//   container.style.display = "flex"
+//   container.style.flexWrap = "wrap"
+//   container.style.border = "3px solid black"
+//   container.style.borderRadius = "3px"
+//
+//   for (let i = 0; i < row * coloumn; i++) {
+//     const gridItem = document.createElement("div")
+//     const itemWidth = 100 / coloumn
+//     gridItem.style.width = `${itemWidth}%`
+//     gridItem.style.border = "1px solid rgba(0, 0, 0, 0.5)"
+//     gridItem.style.boxSizing = "border-box"
+//     gridItem.style.textAlign = "center"
+//
+//     container.appendChild(gridItem)
+//   }
+// }
+//
+//
+// createGrids(16, 16)
