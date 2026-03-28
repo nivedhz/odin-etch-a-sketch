d76280021b81aa87fe8e3f368b525003a2f588aa
Nivedh E R <nivedher12@gmail.com>
2026-03-28 17:13:19 +0530

    refactor(js && css): moved the styling logic of container to css

--- script/script.js
+++ script/script.js
@@ -1,9 +1,5 @@
 function createGrids(row, coloumn) {
   const container = document.getElementById("grid-container");
-  container.style.display = "flex";
-  container.style.flexWrap = "wrap";
-  container.style.border = "3px solid black";
-  container.style.borderRadius = "3px";
 
   for (let i = 0; i < row * coloumn; i++) {
     const gridItem = document.createElement("div");
