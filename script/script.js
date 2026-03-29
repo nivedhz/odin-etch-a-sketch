c7d8035720d741ff8889a80fbf22154ac78f3ad6
Nivedh E R <nivedher12@gmail.com>
2026-03-29 20:53:04 +0530

    feat(core): RGB Mode

--- script/script.js
+++ script/script.js
@@ -2,27 +2,16 @@ const container = document.getElementById("grid-container");
 const clearButton = document.getElementById("clear-button");
 const colorPicker = document.getElementById("color-picker");
 const rgbButton = document.getElementById("rgb-button");
-const randomColors = ["black", "red", "aqua", "azure", "blue", "pink", "peru"];
+const sizeSlider = document.getElementById("size-slider");
+const resetButton = document.getElementById("reset-button");
+let sizeDisplayer = document.getElementById("size-displayer");
 let colorDisplayer = document.getElementById("color-displayer");
+const randomColors = ["black", "red", "aqua", "azure", "blue", "pink", "peru"];
 let selectedColor = "black";
-
-colorPicker.addEventListener("input", (event) => {
-  selectedColor = event.target.value;
-  colorDisplayer.textContent = selectedColor;
-});
-clearButton.addEventListener("click", () => {
-  container.innerHTML = "";
-  createGrid(14);
-});
+const DEFAULT_SIZE = 14;
+let size = DEFAULT_SIZE;
 let isDragging = false;
-window.addEventListener("mousedown", () => {
-  isDragging = true;
-});
-window.addEventListener("mouseup", () => {
-  if (isDragging) {
-    isDragging = false;
-  }
-});
+let rgbMode = false;
 
 function createGrid(value) {
   for (let i = 0; i < value * value; i++) {
@@ -31,17 +20,69 @@ function createGrid(value) {
     gridItem.style.width = `${itemWidth}%`;
     gridItem.style.border = "1px solid rgba(0, 0, 0, 0.3)";
 
-    rgbButton.addEventListener("click", () => {
-      selectedColor =
-        randomColors[Math.floor(Math.random() * randomColors.length - 1)];
-    });
     gridItem.addEventListener("mousemove", () => {
       if (isDragging) {
-        gridItem.style.backgroundColor = selectedColor;
+        if (rgbMode) {
+          gridItem.style.backgroundColor = generateRandomRgb();
+        } else {
+          gridItem.style.backgroundColor = selectedColor;
+        }
       }
     });
     container.appendChild(gridItem);
   }
 }
+
+function generateRandomRgb() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function rgbModeFunction() {
+  rgbMode = !rgbMode;
+  if (rgbMode) {
+    rgbButton.style.color = "white";
+    rgbButton.style.backgroundColor = "black";
+  } else {
+    rgbButton.style.color = "";
+    rgbButton.style.backgroundColor = "";
+  }
+}
+rgbButton.addEventListener("click", rgbModeFunction);
+colorPicker.addEventListener("input", (event) => {
+  selectedColor = event.target.value;
+  colorDisplayer.textContent = selectedColor;
+});
+sizeSlider.addEventListener("input", (event) => {
+  size = event.target.value;
+  sizeDisplayer.textContent = `${size}x${size}`;
+  container.innerHTML = "";
+  createGrid(size);
+});
+clearButton.addEventListener("click", () => {
+  container.innerHTML = "";
+  sizeDisplayer.textContent = `${size}x${size}`;
+  createGrid(size);
+});
+resetButton.addEventListener("click", () => {
+  container.innerHTML = "";
+  colorPicker.value = "#000000";
+  size = DEFAULT_SIZE;
+  rgbMode = false;
+  sizeSlider.value = size;
+  sizeDisplayer.textContent = `${size}x${size}`;
+  colorDisplayer.textContent = "#000000";
+  createGrid(size);
+});
+window.addEventListener("mousedown", () => {
+  isDragging = true;
+});
+window.addEventListener("mouseup", () => {
+  if (isDragging) {
+    isDragging = false;
+  }
+});
 
 createGrid(14);
