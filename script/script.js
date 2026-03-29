9d5237618700df1ccb99de59a8e1eda6f3910c3b
Nivedh E R <nivedher12@gmail.com>
2026-03-29 11:15:12 +0530

    refactor(createGrids): changed the parameters to accept only one value
    -- Changed rows and columns parameter to value
    -- Implemented value inside the function

--- script/script.js
+++ script/script.js
@@ -1,13 +1,15 @@
-function createGrids(row, coloumn) {
+function createGrids(value) {
   const container = document.getElementById("grid-container");
 
-  for (let i = 0; i < row * coloumn; i++) {
+  for (let i = 0; i < value * value; i++) {
     const gridItem = document.createElement("div");
-    const itemWidth = 100 / coloumn;
+    const itemWidth = 100 / value;
     gridItem.style.width = `${itemWidth}%`;
+    gridItem.style.border = "1px solid rgba(0, 0, 0, 0.3)";
 
     container.appendChild(gridItem);
   }
 }
 
-createGrids(128, 128);
+createGrids(14);
+console.log();
