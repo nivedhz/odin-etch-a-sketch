c048fefa67c13e2da44d0ec6360aaf57a1cc7188
Nivedh E R <nivedher12@gmail.com>
2026-03-29 13:19:25 +0530

    add(rgb && clear && core): added the core functionality to the codebase
    -- added the rgb switch and clear switch
    -- implemented the drawing function

--- script/script.js
+++ script/script.js
@@ -1,15 +1,47 @@
-function createGrids(value) {
-  const container = document.getElementById("grid-container");
+const container = document.getElementById("grid-container");
+const clearButton = document.getElementById("clear-button");
+const colorPicker = document.getElementById("color-picker");
+const rgbButton = document.getElementById("rgb-button");
+const randomColors = ["black", "red", "aqua", "azure", "blue", "pink", "peru"];
+let colorDisplayer = document.getElementById("color-displayer");
+let selectedColor = "black";
 
+colorPicker.addEventListener("input", (event) => {
+  selectedColor = event.target.value;
+  colorDisplayer.textContent = selectedColor;
+});
+clearButton.addEventListener("click", () => {
+  container.innerHTML = "";
+  createGrid(14);
+});
+let isDragging = false;
+window.addEventListener("mousedown", () => {
+  isDragging = true;
+});
+window.addEventListener("mouseup", () => {
+  if (isDragging) {
+    isDragging = false;
+  }
+});
+
+function createGrid(value) {
   for (let i = 0; i < value * value; i++) {
-    const gridItem = document.createElement("div");
+    let gridItem = document.createElement("div");
     const itemWidth = 100 / value;
     gridItem.style.width = `${itemWidth}%`;
     gridItem.style.border = "1px solid rgba(0, 0, 0, 0.3)";
 
+    rgbButton.addEventListener("click", () => {
+      selectedColor =
+        randomColors[Math.floor(Math.random() * randomColors.length - 1)];
+    });
+    gridItem.addEventListener("mousemove", () => {
+      if (isDragging) {
+        gridItem.style.backgroundColor = selectedColor;
+      }
+    });
     container.appendChild(gridItem);
   }
 }
 
-createGrids(14);
-console.log();
+createGrid(14);
